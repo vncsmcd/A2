8dcf38ad0305fd43ef74288bcbaffe7eeaf872b6
Vinicius Leal <vmacedopereiraleal@myseneca.ca>
2024-11-22 19:54:16 -0500

    v1.2

--- modules/legoSets.js
+++ modules/legoSets.js
@@ -14,19 +14,18 @@ require('dotenv').config({ path: './db.env' });
 const Sequelize = require('sequelize');
 
 // Initialize Sequelize
-let sequelize = new Sequelize(
-  process.env.DB_DATABASE, 
-  process.env.DB_USER,     
-  process.env.DB_PASSWORD, 
-  {
+const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
-    dialect: 'postgres', 
+    dialect: 'postgres',
     dialectOptions: {
-      ssl: { rejectUnauthorized: false },
+        ssl: {
+            require: true,
+            rejectUnauthorized: false,
+        },
     },
-  }
-);
+});
+
 
 sequelize
   .authenticate()
